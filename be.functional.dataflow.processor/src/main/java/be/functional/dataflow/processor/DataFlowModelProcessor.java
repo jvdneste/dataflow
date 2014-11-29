aef6a6d4ddacb423f6a774ff33e75048f5fbe63a
Johan Van den Neste <jvdneste@gmail.com>
2014-11-29 11:19:01 +0100

    minor changes

--- be.functional.dataflow.processor/src/main/java/be/functional/dataflow/processor/DataFlowModelProcessor.java
+++ be.functional.dataflow.processor/src/main/java/be/functional/dataflow/processor/DataFlowModelProcessor.java
@@ -11,7 +11,6 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.ElementFilter;
 import javax.tools.Diagnostic;
-import javax.tools.Diagnostic.Kind;
 
 import be.functional.dataflow.DataFlowModel;
 
@@ -35,20 +34,16 @@ public class DataFlowModelProcessor extends AbstractProcessor {
 
 	@Override
 	public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
-		processingEnv.getMessager().printMessage(Kind.WARNING, "This is a special warning");
-		if ((annotations.size() == 1)
-				&& Iterables.getOnlyElement(annotations).getQualifiedName().toString().equals(
-						DataFlowModel.class.getName())) {
+		if ((annotations.size() == 1) && Iterables.getOnlyElement(annotations).getQualifiedName().toString().equals(DataFlowModel.class.getName())) {
 			process(roundEnv);
 		}
-		return false;  // never claim annotation, because who knows what other processors want?
+		return false;
 	}
 
 	private void process(final RoundEnvironment roundEnv) {
 		final Collection<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(DataFlowModel.class);
 		final Collection<? extends TypeElement> types = ElementFilter.typesIn(annotatedElements);
 		for (final TypeElement type : types) {
-			processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "DataFlowModelProcessor.process", type);
 			try {
 				processType(type);
 			} catch (final AbortProcessingException e) {
@@ -57,13 +52,17 @@ public class DataFlowModelProcessor extends AbstractProcessor {
 				// Don't propagate this exception, which will confusingly crash the compiler.
 				// Instead, report a compiler error with the stack trace.
 				final String trace = Throwables.getStackTraceAsString(e);
-				reportError("@AutoValue processor threw an exception: " + trace, type);
+				reportError("@DataFlowModel processor threw an exception: " + trace, type);
 			}
 		}
 	}
 
 	private void processType(final TypeElement type) {
+		reportWarning("bla bla" + type.getSimpleName().toString(), type);
+	}
 
+	private void reportWarning(final String msg, final Element e) {
+		processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, msg, e);
 	}
 
 	private void reportError(final String msg, final Element e) {
